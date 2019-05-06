cbbbc9e56614dbdebee6a8527727986e98c20ca2
AriManto <37150326+AriManto@users.noreply.github.com>
2019-05-06 03:48:17 -0300

    Archivos sin usar eliminados

--- archive/JS/annoy.js
+++
@@ -1,8 +0,0 @@
-window.setTimeout(function() {
-
-var answer = prompt("Are we there yet??")
-while(answer.indexOf("yes") === -1){
-  var answer = prompt("Are we there yet??");
-}
-alert("YAY, WE MADE IT!!!");
-  }, 500);
